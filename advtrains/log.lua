d15210603f4654d20bc15be449cdc672dc4d53c5
Gabriel Pérez-Cerezo <gabriel@gpcf.eu>
2018-10-19 10:48:41 +0200

    Move logging over to minetest log.
    This should collaborate nicely with the system log rotate, in addition
    to keeping everything in one place.

--- advtrains/log.lua
+++ advtrains/log.lua
@@ -3,11 +3,7 @@
 advtrains.log = function() end
 
 if minetest.settings:get_bool("advtrains_enable_logging") then
-	advtrains.logfile = advtrains.fpath .. "_log"
-
 	function advtrains.log (event, player, pos, data)
-	   local log = io.open(advtrains.logfile, "a+")
-	   log:write(os.date()..": "..event.." by "..player.." at "..minetest.pos_to_string(pos).." -- "..(data or "").."\n")
-	   log:close()
+		minetest.log("action", "advtrains: " ..event.." by "..player.." at "..minetest.pos_to_string(pos).." -- "..(data or ""))
 	end
 end
