3f16379a67138a45bcd086ee4f36cff2f60f1dda
Y. Wang <yw05@forksworld.de>
2022-11-03 19:53:30 +0100

    Add documentation

--- advtrains/l10n.lua
+++ advtrains/l10n.lua
@@ -1,5 +1,23 @@
+--- Advtrains l10n module.
+-- Advtrains' l10n module is built on top of
+-- [Minetest's](https://minetest.gitlab.io/minetest/translations/).
+-- @module attrans
+-- @alias mt
+
+--- Wrapper for `minetest.translate`.
+-- Note that this function is also called when calling the `attrans`
+-- *table* itself. Doing so is encourged as `attrans.attrans` is only
+-- intended for situations where only functions are accepted.
+-- @function attrans
+-- @param str The string to translate.
+-- @param[opt] ... Additional arguments to pass to `minetest.translate`.
 local S = minetest.get_translator("advtrains")
 
+--- Generate an error message that a user does not have a specific privilege.
+-- @function nopriv
+-- @param priv The privilege that is missing.
+-- @param[opt] verb The action that is denied.
+-- @param[optchain] ... Additional arguments to pass to `minetest.translate`.
 local function S_nopriv(priv, verb, ...)
 	if verb then
 		return S(string.format("You are not allowed to %s without the %s privilege.", verb, priv), ...)
@@ -15,6 +33,7 @@ local mt = {
 }
 
 attrans = {
+	attrans = S,
 	nopriv = S_nopriv,
 }
 setmetatable(attrans, mt)
