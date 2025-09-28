310b610abb6d41c2097da9c72c2452310d63a8ed
orwell <orwell@bleipb.de>
2025-09-28 16:38:13 +0200

    Remove stanizni rozhlas because it doesn't work yet

--- advtrains_line_automation/init.lua
+++ advtrains_line_automation/init.lua
@@ -33,7 +33,7 @@ dofile(modpath.."line_editor.lua")
 dofile(modpath.."scheduler.lua")
 dofile(modpath.."station_editor.lua")
 dofile(modpath.."stoprail.lua")
-dofile(modpath.."station_announcement.lua")
+--dofile(modpath.."station_announcement.lua") -- not yet working
 dofile(modpath.."time_table.lua")
 
 function advtrains.lines.load(data)
